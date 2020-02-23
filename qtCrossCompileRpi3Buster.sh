ed05006cfe31e61aaf6a8509ef4da533708f9b1b
Andy Bond <andy.bond@zoho.com>
2020-02-23 14:01:39 +0000

    Update for qt v5.12.5

--- qtCrossCompileRpi3Buster.sh
+++ qtCrossCompileRpi3Buster.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #Script variables
-QTV="5.10"
-PIV="pi"
+PIV="pi"     #pi version 
+TARGET=pi@pi3dev.local  #Raspberry pi device to use
+
+mkdir ~/raspi
 
 read -p "Shutdown when finished? " -n 1 -r
 echo    # move to a new line
@@ -66,13 +68,13 @@ then
   echo Synchronising sysroot with RPi
   echo ==============================
   echo 'starting /lib (1 of 4)'
-  rsync -az pi@andymote.local:/lib sysroot
+  rsync -avz -e ssh $TARGET:/lib sysroot
   echo 'starting /usr/include (2 of 4)'
-  rsync -az pi@andymote.local:/usr/include sysroot/usr
+  rsync -avz -e ssh $TARGET:/usr/include sysroot/usr
   echo 'starting /usr/lib (3 of 4)'
-  rsync -az pi@andymote.local:/usr/lib sysroot/usr
+  rsync -avz -e ssh $TARGET:/usr/lib sysroot/usr
   echo 'starting /opt/vc (4 of 4)'
-  rsync -az pi@andymote.local:/opt/vc sysroot/opt
+  rsync -avz -e ssh $TARGET:/opt/vc sysroot/opt
   #Next, we need to adjust our symbolic links in sysroot to be relative since this folder structure is in both our computer and Raspberry Pi.
   echo
   date +"%H:%M:%S"
@@ -89,17 +91,18 @@ echo
 date +"%H:%M:%S"
 echo 'Creating clean QT Folder (qtbase)'
 echo =================================
-if [ ! -d /opt/qt5pi/qtbase ]; then
-  git clone git://code.qt.io/qt/qtbase.git -b $QTV
-else
-  cd /opt/qt5pi/qtbase/
-  git clean -dfx
+if [ ! -f /opt/qt5pi/qt-everywhere-src-5.12.5.tar.xz ]; then
+  wget http://download.qt.io/official_releases/qt/5.12/5.12.5/single/qt-everywhere-src-5.12.5.tar.xz
 fi
+rm -Rf /opt/qt5pi/qt-everywhere-src-5.12.5
+tar xvf  qt-everywhere-src-5.12.5.tar.xz
+
+
 
 #In new Raspbian versions [BUSTER is assumed here], EGL libraries have different names than those assumed in Qt configuration files.
 # Edit the qmake.conf file  to fix this; substitute all references to -lEGL and -LGLESv2 for -lbrcmEGL and -lbrcmGLESv2, respectively.
-sed -i 's/-lEGL/-lbrcmEGL/g' /opt/qt5pi/qtbase/mkspecs/devices/linux-rasp-$PIV-g++/qmake.conf
-sed -i 's/-lGLESv2/-lbrcmGLESv2/g' /opt/qt5pi/qtbase/mkspecs/devices/linux-rasp-$PIV-g++/qmake.conf
+sed -i 's/-lEGL/-lbrcmEGL/g' ./qtbase/mkspecs/devices/linux-rasp-$PIV-g++/qmake.conf
+sed -i 's/-lGLESv2/-lbrcmGLESv2/g' ./qtbase/mkspecs/devices/linux-rasp-$PIV-g++/qmake.conf
 
 #Configure Qt for cross compilation
 echo
@@ -108,14 +111,38 @@ echo 'Configuring QT for Cross Compilation'
 echo ====================================1
 rm -Rf /opt/qt5pi/qt5
 rm -Rf /opt/qt5pi/qt5pi
-cd /opt/qt5pi/qtbase/
-./configure -release -opengl es2 -device linux-rasp-$PIV-g++ -device-option CROSS_COMPILE=/opt/qt5pi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf- -sysroot /opt/qt5pi/sysroot -opensource -confirm-license -make libs -prefix /usr/local/qt5pi -extprefix /opt/qt5pi/qt5pi -hostprefix /opt/qt5pi/qt5 -no-use-gold-linker -v -no-gbm
+
+cd  qt-everywhere-src-5.12.5
+#Next Line from: https://mechatronicsblog.com/cross-compile-and-deploy-qt-5-12-for-raspberry-pi/ (modified for this script)
+./configure -release -opengl es2 -device linux-rasp-pi-g++ -device-option CROSS_COMPILE=/opt/qt5pi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf- -sysroot /opt/qt5pi/sysroot -opensource -confirm-license -skip qtwayland -skip qtlocation -skip qtscript -make libs -prefix /usr/local/qt5pi -extprefix /opt/qt5pi/qt5pi -hostprefix /opt/qt5pi/qt5 -no-use-gold-linker -v -no-gbm
+
+
+echo "if configuration was successful you should see a message asking you to run make."
+echo "Also, verify that the build options includes EGFLS for Raspberry Pi."
+echo " "
+echo "Build options:"
+echo "......"
+echo "QPA backends:"
+echo "  ......"
+echo "  EGLFS .................................. yes"
+echo "  EGLFS details:"
+echo "    ......"
+echo "    EGLFS Raspberry Pi ................... yes"
+echo "    ......"
+
+read -p "Do you wish to continue? " -n 1 -r
+echo    # move to a new line
+CONTINUE=$REPLY
+
+if [[ ! $CONTINUE =~ ^[Yy]$ ]]; then
+  exit
+fi
 
 echo
 date +"%H:%M:%S"
 echo 'Running make'
 echo  ============
-make -j7 > ~/raspi/make.log
+make -j15 > ~/raspi/make.log
 
 echo
 date +"%H:%M:%S"
@@ -129,15 +156,16 @@ echo
 date +"%H:%M:%S"
 echo 'Synchronising result with Rpi'
 echo  =============================
-rsync -az qt5pi pi@andymote.local:/usr/local
+rsync -avz -e ssh qt5pi $TARGET:/usr/local
 
 echo
 echo 'Setup for QT Creator'
 echo '===================='
-echo 'C++ Compiler: /opt/qt5pi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++'
-echo 'C Compiler: /opt/qt5pi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc'
-echo 'Debugger: /opt/qt5pi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gdb'
+echo 'C++ Compiler: /opt/qt5pi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/arm-linux-gnueabihf/bin/c++'
+echo 'C Compiler: /opt/qt5pi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/arm-linux-gnueabihf/bin/gcc'
+echo 'Debugger: /opt/qt5pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gdb'
 echo 'QT Version: /opt/qt5pi/qt5/bin/qmake'
+
 
 echo
 date +"%H:%M:%S"
